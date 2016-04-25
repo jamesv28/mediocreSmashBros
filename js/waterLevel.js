d84d0643fa6495b38eef70168442fe31099eff09
Jon Hammond <jonh1016@gmail.com>
2016-04-25 13:59:37 -0600

    Added gravity and collision with ground and palyer

--- js/waterLevel.js
+++ js/waterLevel.js
@@ -1,49 +1,93 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '',
-    {
-        preload: preload,
-        create: create,
-        update: update
-    });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game')
 
-function preload() {
+var PhaserGame = function () {
 
-    game.load.image('background', 'assets/waterBackground.png');
-    game.load.image('floor', 'assets/waterFloor.png');
-    game.load.spritesheet('shark', 'assets/shark1.png', 85, 47);
+    this.bg = null;
+    this.player = null;
+    this.ground = null;
+    this.cursors;
 
+    this.cursors;
+    this.jumpTimer = 0;
+    // this.facing = 'right';
+    // this.locked = false;
+    // this.lockedTo = null;
+    // this.wasLocked = false;
+    // this.willJump = false;
 
-}   //end of preload
+};
 
-//instantiate assets
-var player;
-// var shark;
-var platforms;
-var sand;
+PhaserGame.prototype = {
+  init: function () {
+    this.game.renderer.renderSession.roundPixels = true;
+    // set physics to arcade physics
+    this.physics.startSystem(Phaser.Physics.ARCADE);
+    // world gravity
+    // this.physics.arcade.gravity.y = 600;
+  },
 
-function create() {
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.add.tileSprite(0, 0, 800, 600, 'background');
-    // game.add.tileSprite(0, 900, 640, 20, 'floor');
+  preload: function () {
 
-    sand = game.add.group();
-    sand.enableBody = true;
+      this.load.image('background', 'assets/waterBackground.png');
+      this.load.image('floor', 'assets/waterFloor.png');
+      this.load.spritesheet('shark', 'assets/shark1.png', 85, 47);
+      this.load.spritesheet('mario', 'assets/mariosprite.png', 21, 35);
 
-    var ground = sand.create(0, game.world.height - 50, 'floor');
-    ground.immovable = true;
+  }, //end of preload
 
-    // shark = game.add.group();
-    // shark.enableBody = true;
+  create: function () {
 
-    var baddie = game.add.sprite(200, 200, 'shark')
-    game.physics.arcade.enable(baddie)
-    game.add.physicsGroup()
-    // baddie.addMotionPath([
-    //   { x: "+0", xSpeed: 2000, xEase: "Linear", y: "+300", ySpeed: 2500, yEase: "Sine.easeIn" }
-    // ])
+      this.background = this.add.tileSprite(0, 0, 800, 600, 'background');
+      this.background.fixedToCamera = true;
 
+      this.ground = this.add.tileSprite(0, 600, 800, 20, 'floor');
+      this.ground.immovable = true;
+      this.physics.arcade.enable(this.ground);
+      this.ground.body.collideWorldBounds = true;
 
-}   //end of create
+      this.baddie = this.add.sprite(200, 200, 'shark')
+      this.physics.arcade.enable(this.baddie)
+      this.add.physicsGroup()
+      // baddie.addMotionPath([
+      //   { x: "+0", xSpeed: 2000, xEase: "Linear", y: "+300", ySpeed: 2500, yEase: "Sine.easeIn" }
+      // ])
+      this.cursors = this.input.keyboard.createCursorKeys();
 
-function update() {
+      // Create Player 1
+      this.player = this.add.sprite(32, 0, 'mario')
+      this.physics.arcade.enable(this.player);
+      this.player.body.collideWorldBounds = true;
+      this.player.body.setSize(20, 20, 5, 16);
+      this.player.body.gravity.y = 600;
+      // this.player.physics.arcade.gravity.y = 600;
 
-}   //end of update
+      this.player.animations.add('left', [0, 1, 2, 3, 4], 11, true);
+      this.player.animations.add('turn', [4], 20, true);
+      this.player.animations.add('right', [7, 8, 9, 10, 11], 11, true);
+
+  },   //end of create
+
+  update: function() {
+    this.physics.arcade.collide(this.player, this.stationary);
+    this.physics.arcade.collide(this.player, this.ground);
+    // this.physics.arcade.collide(this.player, this.clouds, this.customSep, null, this);
+
+    //  Do this AFTER the collide check, or we won't have blocked/touching set
+    var standing = this.player.body.blocked.down || this.player.body.touching.down || this.locked;
+
+    this.player.body.velocity.x = 0;
+
+    if (this.cursors.left.isDown) { this.player.body.velocity.x = -150 }
+    else if (this.cursors.right.isDown) { this.player.body.velocity.x = 150 }
+
+    // if (standing && this.cursors.up.isDown) {
+    //     if (this.locked)
+    //     { this.cancelLock() }
+    //     this.willJump = true;}
+    // if (this.locked) {this.checkLock();}
+
+  // game.physics.arcade.overlap(this.player, this.lava, lavaKill, null, this);
+  }   //end of update
+}
+
+game.state.add('Game', PhaserGame, true);
