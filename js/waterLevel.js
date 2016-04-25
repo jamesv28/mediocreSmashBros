0b3c8ea085c18aca2bee9c240e24d1457342c411
Jon Hammond <jonh1016@gmail.com>
2016-04-25 11:54:07 -0600

    Added water level html and water level js

---
+++ js/waterLevel.js
@@ -0,0 +1,24 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '',
+    {
+        preload: preload,
+        create: create,
+        update: update
+    });
+
+function preload() {
+
+    game.load.image('background', 'assets/waterBackground.png');
+
+
+}   //end of preload
+
+function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.add.sprite(0,0,'background');
+
+
+}   //end of create
+
+function update() {
+
+}   //end of update
