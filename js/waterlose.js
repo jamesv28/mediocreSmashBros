d06344af77fd85959080857c993c340465236ebb
Jon Hammond <jonh1016@gmail.com>
2016-04-28 15:35:16 -0600

    Added keypress to return player to games selection page

--- js/waterlose.js
+++ js/waterlose.js
@@ -7,9 +7,15 @@ var loseState = {
 
     var startLabel = game.add.text(80, 200, 'Press the "W" key to try again.', {font: "25px Arial", fill: "#ffffff"})
 
+    var startLabel = game.add.text(80, 300, 'Press the "C" key to play a different game.', {font: "25px Arial", fill: "#ffffff"})
+
     var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
 
     wkey.onDown.addOnce(this.restart, this)
+
+    var ckey = game.input.keyboard.addKey(Phaser.Keyboard.C);
+
+    ckey.onDown.addOnce(this.differentGame, this)
 
   },
 
@@ -17,5 +23,10 @@ var loseState = {
     game.state.start('Game');
     game.state.states.Game.lost = false;
     game.state.states.Game.playerHealth = 100;
+  },
+
+  differentGame: function () {
+    window.location.href = '/'
   }
+  
 }
--- js/waterwin.js
+++ js/waterwin.js
@@ -7,14 +7,25 @@ var winState = {
 
     var startLabel = game.add.text(80, 200, 'Press the "W" key to restart', {font: "25px Arial", fill: "#ffffff"})
 
+    var startLabel = game.add.text(80, 300, 'Press the "C" key to play a different game.', {font: "25px Arial", fill: "#ffffff"})
+
     var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
 
     wkey.onDown.addOnce(this.restart, this)
+
+    var ckey = game.input.keyboard.addKey(Phaser.Keyboard.C);
+
+    ckey.onDown.addOnce(this.differentGame, this)
 
   },
 
   restart: function () {
     game.state.start('Game')
     game.state.states.Game.playerHealth = 100;
+  },
+
+  differentGame: function () {
+    window.location.href = '/'
   }
+
 }
