3ac7742daa435b902748e4dbd47824a479eda6cc
Jon Hammond <jonh1016@gmail.com>
2016-04-28 15:19:26 -0600

    Styled losing and winning states

--- js/waterlose.js
+++ js/waterlose.js
@@ -1,9 +1,11 @@
 var loseState = {
   create: function () {
 
+    var background = game.add.tileSprite(0, 0, 800, 600, 'background')
+
     var loseLabel = game.add.text(80, 80, 'GAME OVER! Try Again?', { font: '25px Arial', fill: '#ffffff'})
 
-    var startLabel = game.add.text(80, game.world.height-80, 'Press the "W" key to try again.', {font: "25px Arial", fill: "#ffffff"})
+    var startLabel = game.add.text(80, 200, 'Press the "W" key to try again.', {font: "25px Arial", fill: "#ffffff"})
 
     var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
 
--- js/waterwin.js
+++ js/waterwin.js
@@ -1,9 +1,11 @@
 var winState = {
   create: function () {
 
+    var background = game.add.tileSprite(0, 0, 800, 600, 'background')
+
     var winLabel = game.add.text(80, 80, 'YOU WON!', { font: '25px Arial', fill: '#ffffff'})
 
-    var startLabel = game.add.text(80, game.world.height-80, 'press the "W" key to restart', {font: "25px Arial", fill: "#ffffff"})
+    var startLabel = game.add.text(80, 200, 'Press the "W" key to restart', {font: "25px Arial", fill: "#ffffff"})
 
     var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
 
